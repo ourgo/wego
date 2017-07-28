842da336bc63db29926fe5dfd7ea644ea3512e8f
xiaoyiyu <xiaqiubo123@163.com>
2017-07-28 09:07:10 +0800

    忽略本地文件

--- main.go
+++ main.go
@@ -2,8 +2,25 @@
 /*main package*/
 package main
 
-import	/*import */
+import	(
+	"fmt"
+	"net/http"
+)
+
+// 展示首页
+func showIndex(write http.ResponseWriter,req *http.Request){
+	// 解析路由参数
+	req.ParseForm()
+
+	//输出hello wego
+	fmt.Fprintf(write,"hello wego")
+
+}
 
 func main(){
 	/*main code*/
+	http.HandleFunc("/",showIndex)
+	fmt.Println("服务启动在8000端口")
+	http.ListenAndServe(":8000",nil)
+
 }

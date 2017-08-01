5e686117ac208d01e562f22a9205926961fbd81a
xiaoyiyu <xiaqiubo123@163.com>
2017-08-01 10:15:31 +0800

    调整查询输出结果

--- model/user/user.go
+++ model/user/user.go
@@ -1,9 +1,8 @@
-package main
+package user
 
 import (
 	"labix.org/v2/mgo"
-	//"labix.org/v2/mgo/bson"
-	"fmt"
+	"labix.org/v2/mgo/bson"
 )
 
 type User struct {
@@ -11,24 +10,38 @@ type User struct {
 	Password string
 }
 
-func FindOne(m map[string]string) {
+func FindOne(m map[string]string) (bson.M,map[string]string) {
 	// 连接数据库
 	Client,_ := mgo.Dial("localhost")
 	// 关闭数据库
-	defer Client.Clone()
+	defer Client.Close()
 	// 设置连接模式
 	Client.SetMode(mgo.Monotonic,true)
 	// 切换连接的数据库DB()和集合C()
 	users := Client.DB("test").C("users")
-	// 查找数据
 	// 定义接收
 	result := &User{}
-	users.Find(m).One(result)
-	fmt.Println(result)
+	// 查找数据
+	err := users.Find(m).One(result)
+	// 错误处理
+	if err != nil{
+		merr := make(map[string]string)
+		merr["code"]="0"
+		merr["msg"]="没有该数据"
+		return nil,merr
+	}
+	// 定义数据转换结构
+	mm := bson.M{}
+	// 转换数据
+	data,_ := bson.Marshal(result)
+	// 转换为map
+	bson.Unmarshal(data,mm)
+	// 返回数据
+	return mm,nil
 }
 
 
-func main() {
+func user() {
 	mm := make(map[string]string)
 	mm["username"]="xiaoli"
 	FindOne(mm)

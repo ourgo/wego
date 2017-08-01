1cc2c5d6ab643db203f64c2241986dc7d9d86602
xiaoyiyu <xiaqiubo123@163.com>
2017-08-01 09:55:46 +0800

    分离model数据

--- model/user/user.go
+++ model/user/user.go
@@ -1,9 +1,35 @@
-package user
+package main
 
 import (
-	""
+	"labix.org/v2/mgo"
+	//"labix.org/v2/mgo/bson"
+	"fmt"
 )
 
-func user() {
+type User struct {
+	Username string
+	Password string
+}
 
+func FindOne(m map[string]string) {
+	// 连接数据库
+	Client,_ := mgo.Dial("localhost")
+	// 关闭数据库
+	defer Client.Clone()
+	// 设置连接模式
+	Client.SetMode(mgo.Monotonic,true)
+	// 切换连接的数据库DB()和集合C()
+	users := Client.DB("test").C("users")
+	// 查找数据
+	// 定义接收
+	result := &User{}
+	users.Find(m).One(result)
+	fmt.Println(result)
+}
+
+
+func main() {
+	mm := make(map[string]string)
+	mm["username"]="xiaoli"
+	FindOne(mm)
 }

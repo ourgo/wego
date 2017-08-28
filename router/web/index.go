dffe2362823848e6c620aae9346bcef3265a3a67
zshangan@icloud.com <zshangan@icloud.com>
2017-08-28 12:11:29 +0800

    modify the index

--- router/web/index.go
+++ router/web/index.go
@@ -2,8 +2,16 @@ package web
 
 import (
 	"net/http"
+	"html/template"
+	"fmt"
 )
 
 func Index(w http.ResponseWriter,r *http.Request) {
-	w.Write([]byte("hello index"))
+	//w.Write([]byte("hello index"))
+	t,err := template.ParseFiles("web/index.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	t.Execute(w,nil)
 }
